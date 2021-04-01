a16d56666d6bc141abd365879f6c4cae75f0ee0e
Chadwell-Clark <wcclark23@comcast.net>
2021-04-01 15:53:42 -0500

    Added customer, location and emplyee directories,cards and css

---
+++ src/components/customer/CustomerCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Customer.css"
+
+export const CustomerCard = () => (
+  <section className="customer">
+    <h3 className="customer__name">Lepton Bringle</h3>
+      <div className="customer__address">Address: 314159 Pi Place</div>
+  </section>
+);
