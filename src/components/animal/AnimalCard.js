5302b11888a0c158909b9c1700ad0eb494c02ead
Chadwell-Clark <wcclark23@comcast.net>
2021-04-02 16:18:45 -0500

    Props, States, NavBar links with Routing, Api and JSON

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Animal.css";
 
 export const AnimalCard = () => (
-  <section className="animal"> {/*   */}
-    <h3 className="animal__name">Doodles</h3>
-    <div className="animal__breed">Breed: Poodle</div>
+  <section className="animal">
+    <h3 className="animal__name">Snowflake</h3>
+    <div className="animal__breed">Breed: Great Pyrenees</div>
   </section>
 );
