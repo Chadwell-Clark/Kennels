2cfa11b7a386d35f1f2ce4fca5519170ef819f42
Chadwell-Clark <wcclark23@comcast.net>
2021-04-04 19:53:39 -0500

    Added phone to CustomerCard and database

--- src/components/customer/CustomerCard.js
+++ src/components/customer/CustomerCard.js
@@ -1,9 +1,10 @@
 import React from "react"
 import "./Customer.css"
 
-export const CustomerCard = ({customer}) => (
+export const CustomerCard = ({ customer }) => (
   <section className="customer">
     <h3 className="customer__name">{customer.name}</h3>
-      <div className="customer__address"> Address: {customer.address}</div>
+    <div className="customer__address">Address: {customer.address}</div>
+    <div className="customer__phone">Phone: {customer.phone}</div>
   </section>
 );
